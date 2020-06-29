7b7a7fc4c81cdd91da3ca87a162dcd1f998ada75
Jonathan Dees <jonathan.dees@sap.com>
2020-06-29 13:08:19 +0200

    Step 2

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ var errReadingInput = errors.New("Error reading input")
 
 func main() {
 
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+	originUnit = strings.ToUpper(os.Args[1])
+
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
